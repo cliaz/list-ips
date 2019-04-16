30134a9757ad9d6e2e6ef1adb64720c6aca63b9f
cliaz <cliaz@users.no.reply.github.com>
2019-04-16 13:00:16 +1000

    initial commit

---
+++ ifp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+external=`curl -s ifconfig.co`
+printf "external:	$external\n"
+
+for p in `ifconfig -l`; do
+	ip=`ifconfig $p | grep 'inet ' | grep -v 127.0.0.1 | cut -d\  -f2`
+	if [ -n "$ip" ]; then
+		printf "$p:	$ip\n"
+	fi
+done
